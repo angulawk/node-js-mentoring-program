c520d2b5238158c922052791b0479d6fd60c5f9d
Agnieszka Wojcik <awojcik@akamai.com>
2020-02-10 09:59:37 +0100

    Use transactions

--- src/task4/loaders/db.ts
+++ src/task4/loaders/db.ts
@@ -1,4 +1,8 @@
+const cls = require("cls-hooked");
 const Sequelize = require("sequelize");
+
+const namespace = cls.createNamespace("nodejs-mentoring");
+Sequelize.useCLS(namespace);
 
 const sequelize = new Sequelize("postgres://uptepqrypkhjar:3239abaa800ca6da4bc102d8356af7f1d8a192233049e738bfbbaab68acb43e7@ec2-54-246-121-32.eu-west-1.compute.amazonaws.com:5432/deh7sc4ufn3si0", {
   dialect: "postgres",
